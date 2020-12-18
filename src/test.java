fd69715d9c7d9d976aaa08471b8e32202930dad4
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2020-12-18 10:07:11 +0200

    Teae väljastamine java abil

--- src/test.java
+++ src/test.java
@@ -1,6 +1,14 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Tere, Java programm!");
-        System.out.println("See on Java IDE katse");
+        System.out.println("Täna õppisime:");
+        ;
+        System.out.println("1. JDK installeerimine ja seadistamine");
+        ;
+        System.out.println("2. IDE installeerimine ja seadistamine");
+        ;
+        System.out.println("3. IDE kasutamine");
+        ;
+        System.out.println("4. git VCS kasutamine");
+        ;
     }
 }
