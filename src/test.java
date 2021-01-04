aeccd19c671518d265ab6d9d576f621e7f1fbace
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-01-04 15:58:50 +0200

    Muutuja ja andmetüübid - ülesanne 1.2

--- src/test.java
+++ src/test.java
@@ -1,9 +1,16 @@
+/*
+ * Muutujate kasutamise näide
+ * autor - Anna Karutina
+ * ülesanne 1.2
+ * */
 public class test {
     public static void main(String[] args) {
-        System.out.println("Täna õppisime:");
-        System.out.println("1. JDK installeerimine ja seadistamine");
-        System.out.println("2. IDE installeerimine ja seadistamine");
-        System.out.println("3. IDE kasutamine");
-        System.out.println("4. git VCS kasutamine");
+        // defineerime muutujad
+        int aasta = 2020;
+        String liblikas = "teelehe-mosaiikliblikas";
+        String lauseKeskosa = ". aasta liblikas on ";
+        String lause = aasta + lauseKeskosa + liblikas;
+        // väljastamine
+        System.out.println(lause);
     }
 }
