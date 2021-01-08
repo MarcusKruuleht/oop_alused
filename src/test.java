e8cb86f937c1e04ca667d7ba8e892217f76a1bd4
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-01-08 12:51:00 +0200

    Peo eelarve - ülesanne 6.3

--- src/test.java
+++ src/test.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 /*
- * meetod + return - näide
- * autor - Anna Karutina
- * ülesanne 6.2
+ * meetod + return
+ * autor - Marcus Kruuleht
+ * ülesanne 6.3
  * */
 
 public class test {
-    // mahlapakkideArv meetod
-    static int mahlapakkideArv(double ounteKogus) {
-        int pakkideArv = (int) Math.round(ounteKogus * 0.4 / 3);
-        return pakkideArv;
+    // eelarve meetod
+    static int eelarve(int külalised) {
+        int arve = (külalised * 10 + 55);
+        return arve;
     }
 
     // main meetod
@@ -19,11 +19,15 @@ public class test {
         // lubame kasutajale sisestada midagi
         Scanner sisend = new Scanner(System.in);
         //selgitame kasutjale, mida tuleb sisestafa
-        System.out.println("Sisestage õunte kogus kilogrammides: ");
+        System.out.println("Mitu külalist on kutsutud? ");
         // salvestame kasutaja sisestatud väärtus
-        double ounteKogus = sisend.nextDouble();
+        int kutsutudKülalised = sisend.nextInt();
         // arvutamine
-        System.out.println(mahlapakkideArv(ounteKogus));
+        System.out.println("Mitu inimest tuleb? ");
+        int tulemasKülalised = sisend.nextInt();
+
+        System.out.println("Maksimaalne eelarve: " + eelarve(kutsutudKülalised) + " eurot");
+        System.out.println("Minimaalne eelarve: " + eelarve(tulemasKülalised) + " eurot");
 
     }
 }
