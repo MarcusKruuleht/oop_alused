af1160d99fd4637b5942ca8ead2967fc360f7412
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-01-11 14:55:51 +0200

    massivid - [] ja {} abil valmistatud

--- src/test.java
+++ src/test.java
@@ -1,37 +1,18 @@
-import java.util.Scanner;
-
 /*
  * meetod + return
  * autor - Marcus Kruuleht
- * ülesanne 6.4a
+ * ülesanne
  * */
 
+
 public class test {
-
-    // eelarve meetod
-
-
-    // main meetod
     public static void main(String[] args) {
-        int kord = 1;
-        int jk = 1;
-        // defineerime muutujad
-        // lubame kasutajale sisestada midagi
-        Scanner sisend = new Scanner(System.in);
-        //selgitame kasutjale, mida tuleb sisestafa
-        System.out.println("Sisestage külaliste arv: ");
-        // salvestame kasutaja sisestatud väärtus
-        int külalisteArv = sisend.nextInt();
-        while (külalisteArv >= kord) {
-            System.out.println("Võõrustaja: \"Tere!\"");
-            System.out.println("Täna " + jk + ". " + "kord tervitada, mõtiskleb võõrustaja.");
-            System.out.println("Külaline: \"Tere, suur tänu kutse eest!\"");
-            jk++;
-            kord++;
-
-
-        }
-
-
+        int[] numbrid = {1, 2, 3, 4, 5};
+        System.out.println(numbrid.length);
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
     }
 }
