ee811c92a41a0ddd4a348ee6bad91fa8727cdd90
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-01-13 13:10:59 +0200

    massivid - Tahvli juurde - 5.4c

--- src/test.java
+++ src/test.java
@@ -1,20 +1,55 @@
 /*
- * meetod + return
- * autor - Marcus Kruuleht
- * ülesanne
+ * autor - Marcus Kruuuleht
+ * ülesanne 5.4c
  * */
 
-
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class test {
     public static void main(String[] args) {
-        int[] vastuvõetud = {2803, 2626, 2359, 1927, 2236, 2281, 2394, 2484, 2468};
-        Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisesta aasta: ");
-        int aasta = sisend.nextInt();
-        int massiiviindeks = aasta - 2011;
-        System.out.println("Vastuvõetud on " + vastuvõetud[massiiviindeks]);
+
+        // massiiv faili sisu hoidmiseks
+
+        ArrayList<String> opilased = new ArrayList<>();
+
+        // määrame fail ja kontrollime, kas on võiamlik lugeda andmed
+
+        File fail = new File("C:\\Users\\marcus\\IdeaProjects\\oop_alused\\src\\nimekiri.txt");
+        Scanner sisendFailist = null;
+        try {
+            sisendFailist = new Scanner(fail);
+        } catch (Exception e) {
+            System.out.println("Faili pole - " + e.getMessage());
+        }
+
+        // loeme failist
+
+        while (sisendFailist.hasNextLine()) {
+            String rida = sisendFailist.nextLine();
+            opilased.add(rida); // lisame loetud väärtus nimekirja sisse
+        }
+        sisendFailist.close();
+        // loome tänase kuupäeva
+        LocalDateTime tananeKuupev = LocalDateTime.now();
+        System.out.println("Täane kuupäev ilma vorminguseta " + tananeKuupev);
+        DateTimeFormatter tananeKuupaevVormindus = DateTimeFormatter.ofPattern("dd");
+        String tananeKuupaevVormindatud = tananeKuupev.format(tananeKuupaevVormindus);
+        System.out.println("Tänane vormintatud kuupäev " + tananeKuupaevVormindatud);
+        // otsime vajalik inimene nimekirjast
+        int indeks = Integer.parseInt(tananeKuupaevVormindatud);
+        System.out.println(opilased.get(indeks - 1));
+        // vaatame nimekirja sisu
+
+        /*
+        for (int i = 0; i < opilased.size(); i++) {
+            System.out.println(i + " " + opilased.get(i));
+         */
+
 
     }
 }
