902d69d5fc08f3b1ec4a836fc5f14d754916aab9
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-02-03 11:09:45 +0200

    Kehamassiindeks

--- src/Inimene.java
+++ src/Inimene.java
@@ -1,10 +1,12 @@
 public class Inimene {
-    public String nimi;
-    private Integer pikkus;
-    private Integer mass;
+    private String nimi;
+    private double pikkus;
+    private double mass;
 
     public Inimene(String nimi) {
         this.nimi = nimi;
+        this.pikkus = 0.0;
+        this.mass = 0.0;
     }
 
     public String getNimi() {
@@ -15,26 +17,63 @@ public class Inimene {
         this.nimi = nimi;
     }
 
-    public int getPikkus() {
+    public double getPikkus() {
         return pikkus;
     }
 
-    public void setPikkus(Integer pikkus) {
+    public void setPikkus(double pikkus) {
         this.pikkus = pikkus;
     }
 
-    public int getMass() {
+    public double getMass() {
         return mass;
     }
 
-    public void setMass(Integer mass) {
+    public void setMass(double mass) {
         this.mass = mass;
     }
 
     public double kmi() {
         double pikkusRuut = (getPikkus() * getPikkus());
-        double indeks = (getMass() / pikkusRuut) * 10000;
+        double indeks = (getMass() / pikkusRuut);
         return indeks;
     }
 
+    public void suurendaKaal() {
+        this.mass++;
+    }
+
+    public void suurendaKaal(double kg) {
+        this.mass = +kg;
+    }
+
+    public void vahendaKaal() {
+        this.mass = -1;
+    }
+
+    public void normaalKaal() {
+        // kui kehamasiiindeks on 19 - 24.9 - see on norm
+        // kui kmi on <19 - tuleb inimest sööta
+        // nii, et ta võtaks 1 kg juurde iga kord
+        // ka kontrollida, kas nüüd asi korras
+        while (this.kmi() < 19.0) {
+            this.suurendaKaal();
+            System.out.printf(getNimi() + ", kehamassiindeks: " + "%.2f%n", kmi());
+            System.out.println(getMass());
+
+            while (this.kmi() > 25.0) {
+                this.vahendaKaal();
+                System.out.printf(getNimi() + ", kehamassiindeks: " + "%.2f%n", kmi());
+                System.out.println(getMass());
+
+                if (this.kmi() >= 19.0 && this.kmi() <= 25.0) {
+                    System.out.println(this.nimi + ", sinu kaal on korras");
+                }
+
+            }
+            // ja juhul kui kaal on 25 ja suurem
+            // siis tuleb pakkuda vähenedada kaalu 1 kg võrra
+
+        }
+    }
 }
